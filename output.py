a9980aff79a7e9dc921caa9f1f9247a814fc6569
Shivam-1702 <97210727+Shivam-1702@users.noreply.github.com>
2022-01-24 15:56:22 +0530

    Update output.py

--- output.py
+++ output.py
@@ -1,8 +1,8 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-========= RESTART: C:\Users\Ranjeeta\Music\py_assign\py2\allquestion.py ========
-Program by CHAITANYA KANHAR CSE 21103127
+========= RESTART: C:\Users\desktop\py_assign\py2\questions(assignment2).py ========
+Program by Shivam dhiman EE 21104062
 WELCOME to the program .
  Hope it will help
 Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
@@ -22,23 +22,23 @@ Part F!
 Pythonisacasesensitivelanguage
 You want to try more or exit??
 'Y' for yes, 'N' for no y
-Program by CHAITANYA KANHAR CSE 21103127
+Program by Shivam dhiman EE 21104062
 WELCOME to the program again.
  Hope it will help
 Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
 Enter your choice here: 2
-Please enter your name: Chaitanya
-Enter SID: 21103127
-Enter department name: cse
+Please enter your name: Shivam dhiman
+Enter SID: 21104062
+Enter department name: EE
 Enter CGPA: 10
 
     
-Hey, Chaitanya Here!
-             My SID is 21103127
-             I am from cse department and my CGPA is 10.0
+Hey, shivam Here!
+             My SID is 21104062
+             I am from EE department and my CGPA is 10.0
 You want to try more or exit??
 'Y' for yes, 'N' for no y
-Program by CHAITANYA KANHAR CSE 21103127
+Program by  Shivam dhiman EE 21104062
 WELCOME to the program again.
  Hope it will help
 Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
@@ -59,7 +59,7 @@ Part E!
 a>>2: 14 and b>>4 0
 You want to try more or exit??
 'Y' for yes, 'N' for no y
-Program by CHAITANYA KANHAR CSE 21103127
+Program by Shivam dhiman EE 21104062
 WELCOME to the program again.
  Hope it will help
 Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
@@ -70,7 +70,7 @@ Enter number 3: 33
 The greatest value is: 33.0
 You want to try more or exit??
 'Y' for yes, 'N' for no y
-Program by CHAITANYA KANHAR CSE 21103127
+Program by Shivam dhiman EE 21104062
 WELCOME to the program again.
  Hope it will help
 Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
@@ -79,7 +79,7 @@ Enter the string: hello guys whats your name
 yes
 You want to try more or exit??
 'Y' for yes, 'N' for no y
-Program by CHAITANYA KANHAR CSE 21103127
+Program by Shivam dhiman EE 21104062
 WELCOME to the program again.
  Hope it will help
 Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
